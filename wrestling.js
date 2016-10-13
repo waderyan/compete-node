bcad97bbf6530fa6ebe2df65725035e8b6188b38
Wade Anderson <waander@Wades-MacBook-Pro.local>
2016-10-13 11:27:18 -0700

    prepping for demo

--- wrestling.js
+++ wrestling.js
@@ -3,17 +3,16 @@
 var Promise = require('promise');
 var _ = require('lodash');
 
-
 function getScore(product) {
     return _.reduce(product.factors, function (sum, x) {
-        return sum + x * (Math.random() * 10);
+        //  * (Math.random() * 10)
+        return sum + x;
     });
 }
 
 module.exports = {
     runMatch: function(xProduct, yProduct) {
-
-        return new Promise(function(resolve) {
+        return new Promise(function(resolve, reject) {
             var xKarma = getScore(xProduct);
             var yKarma = getScore(yProduct);
 
